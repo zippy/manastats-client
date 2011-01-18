698a7758d3bee9464c1c1a93cb30b62a5588b5ab
Eric Harris-Braun <eric@harris-braun.com>
2011-01-18 11:25:43 -0500

    blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManastatsClient::Application.config.secret_token = '72f5443994ade080e33e4153da00b0418c006c84a2026fb3ba04da155caed2d3e8f241c5f5f5724c5cbafa39095037344ab7413ca48bcb0ae2984964800ee67c'
